c15813b31cfaadf6fd7e561fe2779639e586483f
Devenford <khaushldev.ls@gmail.com>
2026-03-30 21:14:18 +0530

    added eslint config file

--- backend/index.js
+++ backend/index.js
@@ -28,15 +28,15 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
-  .then(note => {
-    if(note) {
-      response.json(note)
-    }
-    else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(note => {
+      if(note) {
+        response.json(note)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
@@ -48,42 +48,42 @@ app.post('/api/notes', (request, response, next) => {
   })
 
   note.save()
-  .then(savedNote => {
-    response.json(savedNote)
-  })
-  .catch(error => next(error))
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
   const {content, important} = request.body
 
   Note.findById(request.params.id)
-  .then(note => {
-    if(!note) {
-      return response.status(404).end()
-    }
+    .then(note => {
+      if(!note) {
+        return response.status(404).end()
+      }
 
-    note.content = content
-    note.important = important
+      note.content = content
+      note.important = important
 
-    return note.save().then(updatedNote => {
-      response.json(updatedNote)
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
     })
-  })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-  .then(result => {
-    if(result) {
-      response.status(204).end()
-    }
-    else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(result => {
+      if(result) {
+        response.status(204).end()
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
--- backend/models/note.js
+++ backend/models/note.js
@@ -5,20 +5,20 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.set('strictQuery', false)
 mongoose.connect(url, {family: 4})
-    .then(result => {
-      console.log('connected to MongoDB')
-    })
-    .catch(error => {
-      console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minLength: 5,
-      required: true
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {

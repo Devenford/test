3cddb1ea9ec3f493c92b326f56b3a25f260df9db
Devenford <khaushldev.ls@gmail.com>
2026-06-14 11:16:41 +0530

    upadated frontend

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,6 @@ const loginRouter = require('./controllers/login')
 
 const app = express()
 
-logger.info('connecting to', config.MONGODB_URI)
-
 mongoose
   .connect(config.MONGODB_URI, { family: 4 })
   .then(() => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import loginService from './services/login'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 
@@ -9,15 +10,17 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
-  const hook = () => {
+  useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => {
         setNotes(initialNotes)
       })
-  }
-  useEffect(hook, [])
+  }, [])
 
   const notesToShow = showAll ?  notes : notes.filter((note) => note.important === true)
 
@@ -58,18 +61,61 @@ const App = () => {
         })
   }
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input 
+            type='text' 
+            value={username} 
+            onChange={({ target }) => setUsername(target.value)} 
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+            type='text'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
+      </div>
       <ul>
         {notesToShow.map(note => 
           <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
-      </div>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
